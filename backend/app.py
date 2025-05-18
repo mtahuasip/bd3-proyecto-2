634f2d8dcdf69a80dc9b08228444ba47ca678ced
Matt Logan Tahuasi Pariamo <mtahuasip@fcpn.edu.bo>
2025-05-17 23:42:40 -0400

    No se realizaron cambios en el código.

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,7 @@
+import os
 from src import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    from src.config import Config
-
-    app.run(port=Config.PORT, debug=Config.DEBUG)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
